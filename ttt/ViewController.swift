0933a470f075dbd317c450fb6a8011224d26af4b
Faisal Alsaadi <faisal.jo.ksa@gmail.com>
2019-01-08 16:21:45 +0200

    first init

---
+++ ttt/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ttt
+//
+//  Created by faisal on 1/8/19.
+//  Copyright © 2019 Apptrainers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("asd")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
